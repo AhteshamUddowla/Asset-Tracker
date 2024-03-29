4a8578dcc502d50d850d51e53b8dcc7a34fea9bc
AhteshamUddowla <ahteshamiiuc@gmail.com>
2024-03-29 19:49:09 +0600

    register view created

--- assets/views.py
+++ assets/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Asset
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def login_view(request):
@@ -28,11 +29,27 @@ def login_view(request):
 
     return render(request, 'login.html')
 
+def signup_view(request):
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            login(request, user)
+            return redirect('assets')
+        else:
+            messages.error(request, 'An error has occured')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
 def logout_view(request):
     logout(request)
     messages.info(request, "User was logged out!")
     return redirect('login')
-
 
 def assets_view(request):
     assets = Asset.objects.all()
